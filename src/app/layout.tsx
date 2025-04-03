2d760b2e4954256bf3543cd9c9ea9feb315b8c63
AndriyPapirnyk <papirnykandriy@gmail.com>
2025-04-03 22:01:54 +0300

    adding header  to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import Header from "./components/header/Header";
+import "./reset.css"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,6 +25,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Header />
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         {children}
       </body>
