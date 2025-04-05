218e5e3880084d75a6fa1e7ef9f4d397d7b5db01
Ostap Prodanyuk <OstapoKapo@gmail.com>
2025-04-06 02:32:30 +0300

    create some part of main page

--- src/app/components/header/Header.tsx
+++ src/app/components/header/Header.tsx
@@ -2,6 +2,10 @@
 import "./Header.scss"
 import Image from "next/image"
 import logo from "../../../../public/icons/logo.png"
+import searchIcon from "../../../../public/icons/search.png"
+import cartIcon from "../../../../public/icons/cart.png"
+import heartIcon from "../../../../public/icons/heart.png"
+import profileIcon from "../../../../public/icons/profile.png"
 import Button from "../button/Button"
 import { useEffect, useState } from "react";
 import { useRouter } from "next/router"
@@ -11,7 +15,6 @@ import Link from "next/link"
 
 export default function Header() {
 
-  const [signKey, setSignKey] = useState<boolean>(false);
   const pathname = usePathname();
 
   
@@ -25,14 +28,41 @@ export default function Header() {
         <div className="header__logo"> 
             <Image src={logo} alt="Logo" width={130} height={40}/>
         </div>
-        <nav className="header__nav">
+        {(pathname == '/login' || pathname == '/signin') ? <nav className="header__nav">
             <Link href={'/login'}>
-              <Button text={'Login'} buttonStyle={pathname == '/LogIn' ? 1 : 2} />
+              <Button text={'Login'} buttonStyle={pathname == '/login' ? 1 : 2} />
             </Link>
-            <Link href={'/signIn'}>
-              <Button text={'Sign Up'} buttonStyle={pathname == '/SignIn' ? 1 : 2} />
+            <Link href={'/signin'}>
+              <Button text={'Sign Up'} buttonStyle={pathname == '/signin' ? 1 : 2} />
             </Link>
-        </nav>
+        </nav> : null}
+        {pathname == '/' ? (
+          <div className="header__content">
+           <ul>
+              <li className="header__content-li_active">Shop</li>
+              <li className="header__content-li" >Men</li>
+              <li className="header__content-li">Women</li>
+              <li className="header__content-li">Combos</li>
+              <li className="header__content-li">Joggers</li>
+           </ul>
+           <div className="header__content-search">
+              <Image src={searchIcon} width={20} height={20} alt="search button"></Image>
+              <input type="text" className="" placeholder="Search" />
+           </div>
+           <div className="header__content-buttons">
+             <div>
+               <Image src={heartIcon} width={25} height={25} alt="heart"></Image>
+             </div>
+             <div>
+               <Image src={profileIcon} width={25} height={25} alt="profile"></Image>
+             </div>
+             <div>
+               <Image src={cartIcon} width={25} height={25} alt="cart"></Image>
+             </div>        
+            </div>
+          </div>
+        )
+        : null}
     </header>
   )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,80 @@
-export default function Home() {
+'use client'
+import './page.scss';
+import Image from "next/image";
+import sliderNext from "../../public/icons/sliderNext.png";
+import sliderPrev from "../../public/icons/sliderPrev.png";
+import { useRef, useState } from 'react';
 
-return (
-    <>Hello</>
-  );
+interface SliderContent {
+  subtitle: string;
+  title: string;
+  p?: string;
+  buttonText: string;
 }
+
+const Home: React.FC = () => {
+
+  const pagination =  useRef<HTMLDivElement>(null);
+  const [sliderPage, setSliderPage] = useState<number>(0);
+
+  const slider: SliderContent[] = [{
+    subtitle: 'T-shirt / Tops',
+    title: 'Summer Value Pack',
+    p: 'cool / colorful / comfy',
+    buttonText: 'Shop Now',
+  },{
+    p: 'rah / bad / bitch',
+    title: 'Summer Value Pack',
+    subtitle: 'Sweatshirt / Hoodies',
+    buttonText: 'Shop Now',
+  }]
+
+  const hadnlePaginationSliderPage = (e: React.MouseEvent<HTMLImageElement>) => {
+    const target = e.target as HTMLDivElement;
+    if(target.getAttribute('data-action')){
+      if(target.getAttribute('data-action') == 'prev'){
+        if(sliderPage !== 0){
+          setSliderPage(sliderPage - 1);
+        }
+      }else if(target.getAttribute('data-action') == 'next'){
+        if(sliderPage !== slider.length - 1){
+          setSliderPage(sliderPage + 1);
+        }
+      }else if(target.getAttribute('data-action') == '0' || '1'){
+        setSliderPage(parseInt(target.getAttribute('data-action') ?? ''));
+      }
+    }
+  }
+
+
+  return (
+    <div className='home'>
+      <div className="home__slider" style={{backgroundImage: `url(/background/sliderPage${sliderPage+1}.jpg)`}} onClick={hadnlePaginationSliderPage}>
+        <Image data-action='prev' src={sliderPrev} width={30} height={30} alt={'prev'}></Image>
+        <div className="home__slider-container">
+          {slider.map((item:SliderContent, index:number)=> 
+        <div className='home__slider-content' style={{display: sliderPage == index ? 'flex' : 'none'}} key={index}>
+          <div>
+            <p>{item.subtitle}</p>
+            <h1> {item.title}</h1>
+            <p>{item.p}</p>
+            <button>{item.buttonText}</button>
+          </div>
+          <div></div>
+        </div>
+          )}
+          <div className='home__slider-paginations' ref={pagination}>
+            <div data-action='0' style={{opacity: sliderPage == 0 ? '1' : '0.5'}} className='home__slider-pagination home__slider-pagination_left'></div>
+            <div data-action='1' style={{opacity: sliderPage == 1 ? '1' : '0.5'}} className='home__slider-pagination home__slider-pagination_right '></div>
+          </div>
+        </div>
+        <Image  data-action='next' src={sliderNext} width={30} height={30} alt={'next'}></Image>
+      </div>
+      <div className="home__container">
+       
+      </div>
+    </div>
+  )
+}
+
+export default Home;
